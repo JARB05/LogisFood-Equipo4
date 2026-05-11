1a4681a853f698cfa2d59519ba58813ed0478068
Juan Francisco García Cortés <juanfran052602@gmail.com>
2026-05-11 12:41:27 -0600

    feature: controller que resuelve el mixed content y los assets cargarán por HTTPS correctamente

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'rol' => \App\Http\Middleware\CheckRol::class,
         ]);
     })
+    ->withMiddleware(function (Middleware $middleware) {
+    $middleware->trustProxies(at: '*');
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
